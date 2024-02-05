8534a20aabbecf9b880f7730d8abbb558781d2ff
Harsha Goel <harshagoyal868@gmail.com>
2024-02-05 16:41:10 +0530

    first commit

---
+++ custom.d.ts
@@ -0,0 +1,4 @@
+declare module 'use-sound' {
+    const useSound: (...args: any[]) => any; // Adjust this according to the actual useSound signature
+    export = useSound;
+  }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains: [
+            "xukrclpojkcgsuhdmjcz.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
+
+
+
